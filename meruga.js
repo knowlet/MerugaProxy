7c9feeeaca4bbe44a6220c2e3ed67e2264e3bb75
Knowlet <knowlet3389@gmail.com>
2014-11-22 17:51:09 +0800

    add new functions, config_wave, open_album

--- meruga.js
+++ meruga.js
@@ -1,4 +1,4 @@
-var port = 8070;
+var port = process.argv[2] || 8070;
 var mult = 1000;
 var proxy = require('proxy-tamper').start({port: port});
 var crypto = require('crypto');
@@ -6,15 +6,17 @@ var crypto = require('crypto');
 function log(str) {
   console.log(' [' + new Date().toLocaleTimeString() + '] ' + str);
 }
+
+log('Proxy Start at port: ' + port);
 // block other website
-// proxy.tamper(/^((?!toto)[\s\S])*$/, "Sorry, The Proxy is for Meruga Only.");
+// proxy.tamper(/^((?!toto)[\s\S])*$/, 'Sorry, The Proxy is for Meruga Only.');
 
 proxy.tamper(/toto-taiwan.hekk.org\/users\/preset_data.json/, function (request) {
   log('Device connect: ' + request.headers.device_info.split(':::')[0]);
   delete request.headers['accept-encoding'];
   request.onResponse(function (response) {
     try {
-      var resJson = JSON.parse(response.body.toString('utf8').escapeSpecialChars());
+      var resJson = JSON.parse(response.body);
       log('Welcome User ' + resJson.data.user.name);
       resJson.data.infos.unshift({"body": "癒術士大大安安~\r\n外掛已經成功連線了喔\r\n目前修改倍率為 " + mult + " 倍喔啾咪 O_<", "categories": [], "created_at": new Date().toISOString(), "deleted_at": null, "id": 0, "rich_body": "", "title": "外掛連線通知", "updated_at": new Date().toISOString(), "visible": true, "wordpress_id": 0, "created_sec": 0, "updated_sec": 0});
       response.body = JSON.stringify(resJson);
@@ -25,12 +27,12 @@ proxy.tamper(/toto-taiwan.hekk.org\/users\/preset_data.json/, function (request)
     response.complete();
   });
 });
-
+/*
 proxy.tamper(/toto-taiwan.hekk.org\/users\/messages/, function (request) {
   delete request.headers['accept-encoding'];
   request.onResponse(function (response) {
     try {
-      var resJson = JSON.parse(response.body.toString('utf8').escapeSpecialChars());
+      var resJson = JSON.parse(response.body);
       if (typeof(resJson.data.messages.user_comment) !== 'object') resJson.data.messages.user_comment = [];
       var id = crypto.randomBytes(4).toString('hex') + '-' + crypto.randomBytes(2).toString('hex') + '-' + crypto.randomBytes(2).toString('hex') + '-' + crypto.randomBytes(2).toString('hex') + '-' + crypto.randomBytes(6).toString('hex');
       resJson.data.messages.user_comment.unshift({"user_id":"","unit_id": 0,"user_name":"小精靈","text":"外掛使用中 O_<","type_id":"user_comment","id":id,"created_at":parseInt(new Date().getTime()/1000)});
@@ -42,6 +44,22 @@ proxy.tamper(/toto-taiwan.hekk.org\/users\/messages/, function (request) {
     response.complete();
   });
 });
+*/
+// Show all album
+proxy.tamper(/toto-taiwan.hekk.org\/albums\/$/, function (request) {
+  delete request.headers['accept-encoding'];
+  request.onResponse(function (response) {
+    var resJson = JSON.parse(response.body);
+    resJson.data.album_data.count = resJson.data.album_data.total_count;
+    for (i in resJson.data.albums) {
+      resJson.data.albums[i].is_user_unit = true;
+      resJson.data.albums[i].is_waiting = true;
+      resJson.data.albums[i].hide = false;
+    }
+    response.body = JSON.stringify(resJson);
+    response.complete();
+  });
+});
 
 // debug
 /*
@@ -49,16 +67,9 @@ proxy.tamper(/./, function (request) {
   console.log('debug ' + request.url);
 });
 */
-String.prototype.escapeSpecialChars = function() {
-    return this.replace(/\\n/g, "\\n")
-               .replace(/\\'/g, "\\'")
-               .replace(/\\"/g, '\\"')
-               .replace(/\\&/g, "\\&")
-               .replace(/\\r/g, "\\r")
-               .replace(/\\t/g, "\\t")
-               .replace(/\\b/g, "\\b")
-               .replace(/\\f/g, "\\f");
-};
+
+// offline game mode.
+// proxy.tamper(/toto-taiwan.hekk.org\/quests\/ap_use/, '{"status":"success"}');
 
 proxy.tamper(/toto-taiwan.hekk.org.*execute.*json/, function (request) {
   log('tampering ' + request.url.split('?')[0]);
@@ -68,15 +79,26 @@ proxy.tamper(/toto-taiwan.hekk.org.*execute.*json/, function (request) {
 
   request.onResponse(function (response) {
     // tamper the body
+    Object.prototype.removeWave = function() {
+      for (e in this) {
+        if (this.hasOwnProperty(e)) {
+          // if (e === '1') continue;
+          // delete this[e];
+          this[e].length = 0;
+        }
+      }
+    };
     try {
-      var resJson = JSON.parse(response.body.toString('utf8').escapeSpecialChars());
+      var resJson = JSON.parse(response.body);
       if (response.body.search('modal') != -1)
         log(resJson.modal.body.replace(/\n/, ' '));
       else {
         log(resJson.data.user.name + '進場ing...')
         log('hacking sp point!');
-        // console.log(response.body.toString('utf8'));
         resJson.units[0].bonus_sp *= mult;
+        log('hacking wave_conf');
+        resJson.quest.wave_conf.removeWave();
+        // resJson.quest.wave_conf['1'].length /= 10;
         response.body = JSON.stringify(resJson);
       }
     } catch (err) {
